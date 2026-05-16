41b1a41fe83bb4e7e4acd13f461e96c44f9a0617
davidmro150-star <davidmro150@gmail.com>
2026-05-16 23:30:49 +0600

    updated

--- src/components/shop/Allproducts.jsx
+++ src/components/shop/Allproducts.jsx
@@ -8,11 +8,19 @@ const Allproducts = ({items, view}) => {
     <section>
       <Container>
       
-        <div className= {`grid grid-cols-${view} gap-x-7.5 gap-y-15`}>
-                 {items?.map((item) => (
-          <Product item={item} />
-        ))}
-        </div>
+   <div
+  className={`grid gap-x-7.5 gap-y-15 ${
+    view === 2
+      ? "grid-cols-2"
+      : view === 3
+      ? "grid-cols-3"
+      : "grid-cols-4"
+  }`}
+>
+  {items?.map((item) => (
+    <Product key={item.id} item={item} />
+  ))}
+</div>
    
           
       
--- src/components/shop/SortAndView.jsx
+++ src/components/shop/SortAndView.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoFilterSharp } from "react-icons/io5";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SortAndView = ({setView}) => {
 
@@ -8,9 +9,11 @@ const SortAndView = ({setView}) => {
     setView(view)
     
   )
+
+  const[filterModal, setFilterModal] = useState(false)
   return (
  
-    <div className="relative flex  gap-15.5 after:absolute after:top-0 after:-right-7.5 after:h-5.5 after:w-0.5 after:bg-[#E4E4E4]  after:content-[''] ">
+    <div className="relative  flex  gap-15.5 after:absolute after:top-0 after:-right-7.5 after:h-5.5 after:w-0.5 after:bg-[#E4E4E4]  after:content-[''] ">
       <select className='font-jost font-medium text-primary-black text-base leading-6 border-b-2 uppercase'>
         
       <option className='bg-primary-black text-white corsure-pointer'>DEFAULT SORTING</option>
@@ -35,10 +38,27 @@ const SortAndView = ({setView}) => {
 
     
       </div>
-      <div className='flex gap-2.5'>
+      <div onClick={()=>setFilterModal(!filterModal)} className='flex gap-2.5 items-center cursor-pointer'>
         <IoFilterSharp />
-         <h2 className='font-jost font-medium text-primary-black text-base leading-6 uppercase'>Filter</h2>
+        <h2 className='font-jost font-medium text-primary-black text-base leading-6 uppercase'>Filter</h2>
+        
       </div>
+
+ 
+      <div className={`fixed z-50 duration-300 ${filterModal ? "w-105" : "w-0"} h-full bg-gray-500 absolute right-0 top-0`}>
+
+        <div className='bg-[#E4E4E4] px-10'>
+                  <div className='flex items-center justify-between'>
+            <h3 className='font-jost font-medium text-base text-primary-black'>FILTER BY</h3>
+      
+             <AiOutlineClose onClick={()=>setFilterModal(false)} size={15} />
+   </div>
+    </div>
+      </div>
+      
+  
+
+     
    </div>
 
   )
